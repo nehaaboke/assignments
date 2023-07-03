6f60b11664e02c28761b2bc4286b6c001f610857
nehaaboke <137645500+nehaaboke@users.noreply.github.com>
2023-07-03 20:38:47 +0530

    Add files via upload

---
+++ assignment_9.py
@@ -0,0 +1,16 @@
+import math_opr as m
+# Create an instance of MathOperations
+opr = m.MathOperations()
+
+# Call member functions
+add = opr.add()
+sub= opr.subtract()
+mul = opr.multiply()
+div = opr.divide()
+sqr = opr.square_root()
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+print("Square Root:", sqr)
